24d6978e5278b44b097720cab25834ebf02a83ab
craig91 <craigdejean1@gmail.com>
2016-01-22 21:08:04 -0500

    Java_pt.1

---
+++ Java#1.js
@@ -0,0 +1,16 @@
+var cat = 4;
+// var stands for variable and cat is giving the variable a name. Once the name is declared then you assign it a value, in this case cat = 4
+var price; 
+var quantity 
+var total
+
+price = 5;
+quantity = 14;
+total = price * quantity
+// Here you can see the structure in which assigned the variables values. 3 Variables are assigned names and values of a process that determines the price 
+// of a product. The total is the price variable multiplied by the quantitiy variable.
+
+var el = document.getElementByID('cost');
+// 
+el.textContent = '$' + total; 
+
