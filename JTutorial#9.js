4deb2013e3b6a7089c2be1f3853bb2ca5d32468f
Craig Dejean <DevOps@Craigs-MacBook-Air.local>
2016-05-09 23:33:13 -0400

    Java_pt.10

---
+++ JTutorial#9.js
@@ -0,0 +1,28 @@
+// var today = new Date();
+// var hourNow = today.getHours();
+// var greeting;
+
+// if (hourNow > 18){
+// 	greeting = 'Good evening!';
+// } else if (hourNow > 12) {
+// 	greeting =  'Good afternoon!';
+// } else if (hourNow > 0) {
+// 	greeting = 'Good morning';
+// } else {
+// 	greeting = 'Welcome';
+// }
+
+document.write ('<h3>' + greeting + '</h3>' );
+
+
+// [document] is the object. It represents the entire web page.
+
+// [.] is the Member operator. The docuement has several methods and properties. 
+// [.]cont..  They are known as members of that object.
+
+// The write() method of the document object allows new contet to be written into 
+// the page where the <script> element sits. 
+
+//() are parameters. Whenever a method requires some information to work, the data
+// is given inside the (). Each piece of info is called a parameter of the method.
+// In this case, the write() method needs to know what to write into the page.
