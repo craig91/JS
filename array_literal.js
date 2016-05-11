4b9c52651e35b74ca15a3ddba115b8a0dbd76741
craig91 <craigdejean1@gmail.com>
2016-05-10 23:06:11 -0400

    array_literal

---
+++ array_literal.js
@@ -0,0 +1,11 @@
+var colors;
+colors = ['white', 'black', 'custom'];
+
+var el = document.getElementById('colors');
+el.textContent = colors[0];
+
+
+// This is an array literal. The values are assigned to the array inside a pair 
+// of square brackets and each value is seperated by a comma. The values in the array
+// do not need to be the same data type, so you can store a string,number and a boolean 
+// in the same array.
