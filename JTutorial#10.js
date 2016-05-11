5cc9e92609fd2af006d53bc07b59f6026036a166
Craig Dejean <DevOps@Craigs-MacBook-Air.local>
2016-05-10 23:03:32 -0400

    Java_pt.11

---
+++ JTutorial#10.js
@@ -0,0 +1,33 @@
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+
+var el = document.getElementById('cost');
+el = textContent = '$' + total;
+
+
+// variables are given names to store values.
+// var (variable) price(name of the variable) price = 5; (the variable price is given a value)
+// Here are 3 different data types that can be represented by a variable.
+
+
+// Numeric 
+var price = 5;
+
+
+// String 
+var username;
+username = 'Jeff'
+
+
+// Boolean
+var hungry;
+var thirsty;
+
+hungry = true
+thirsty = false
