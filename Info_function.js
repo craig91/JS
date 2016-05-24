aba90b6870425c2968f529496d7aa6dabd190b52
craig91 <craigdejean1@gmail.com>
2016-05-24 15:33:54 -0400

    Functions_pt.3

---
+++ Info_function.js
@@ -0,0 +1,12 @@
+function getArea(width, height) {
+	return width * height;
+}
+
+/* Sometimes a function needs specific information to perform its task. 
+In such cases, when you declare the function you give its parameters.
+Inside the function, the parameters act like variables.
+
+If a function needs information to work, you indicate what it needs to know in parentheses after the function name.
+The (width, height) is the info it needs to work, this information is known as the PARAMETERS of the function.
+
+ 
