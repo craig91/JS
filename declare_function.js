8385caf1fa935c43a2a4b524de0f4568d812be47
craig91 <craigdejean1@gmail.com>
2016-05-24 15:11:53 -0400

    Functions_pt.2

---
+++ declare_function.js
@@ -0,0 +1,17 @@
+function sayHello() {
+	document.write('Hello!');
+	}
+
+/* To create a function, you give it a name and then write the statements needed to achieve
+ its task inside the curly braces.
+
+ You declare a function using the function key word
+ You give the funtion a name (sometimes called an identifier) followed by parentheses
+ in this case sayHello() is the name of the funtion.
+
+ The statements that perform the task sit inside a code block
+ sayHello(){
+	document.write('Hello')
+ }
+ The code block is represented by the curly braces while the statements to be executed
+ sit inside of it.
