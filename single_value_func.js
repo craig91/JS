628a55ebb4d35efdf462e5440d542a3d762f96ee
craig91 <craigdejean1@gmail.com>
2016-05-26 14:10:54 -0400

    Functions_pt.4

---
+++ single_value_func.js
@@ -0,0 +1,11 @@
+function calculateArea(width, height); {
+	var area =  width * height
+	return area;
+}
+var wallOne = calculateArea(3, 5);
+var wallTwo = calculateArea(8, 5);
+
+
+/* Getting a single value out of a function
+creating and assigning values to the variables (wallOne and wallTwo) using the 
+calculateArea function to return a single value */
