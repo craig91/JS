1bbed8065348cbcb53b424248e57fb1e17882d8a
craig91 <craigdejean1@gmail.com>
2016-06-06 12:51:42 -0400

    if_Statement

---
+++ if_statement.js
@@ -0,0 +1,6 @@
+if ("Benny".length < 55){
+	console.log("You have a short name!");
+	}
+
+	/* An if statement is made up of the if keyword. An if statement executes a 
+	statement if a specified condition is true.
