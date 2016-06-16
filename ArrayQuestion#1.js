5d190b486c0a027d557712cc3a1fb606e27239a6
craig91 <craigdejean1@gmail.com>
2016-06-16 15:27:03 -0400

    Array_Question

---
+++ ArrayQuestion#1.js
@@ -0,0 +1,30 @@
+// Write the code that reads the second value out of an array names myArray:
+
+var myFavoriteNumber = 77
+var yourFavoriteNumber = 66
+if (myFavoriteNumber > yourFavoriteNumber) {
+	console.log("A")
+} else {
+	console.log("B")
+}
+console.log("C")
+
+
+/* When trying to figure this problem out, you must read the question carefully. Even though there
+is no array shown, you can see that because of the 2 variables, you can make an array for both of
+the 2 values of the variables, so: */
+ 
+ var myArray = [66,77] 
+
+ // The if statement is asking weather myFavoriteNumber (77) is larger than yourFavoriteNumber (66)
+
+ if (myFavoriteNumber > yourFavoriteNumber){
+
+ }
+
+ /* 77 is greater than 66 so this is true.}else{ } is asking if myFavoriteNumber
+ > yourFavoriteNumber returns false, then ("B") will be printed to the console. 
+ Since this is not the case, ("A") will be printed to the console. Since this
+ is true, ("C") will always print to the console.
+
+
