26259290db28ba303233f594ed8a519a164acdd6
craig91 <craigdejean1@gmail.com>
2016-07-05 15:57:27 -0400

    update

--- Objects_pt.1.js
+++ Objects_pt.1.js
@@ -1,5 +1,5 @@
 var hotel = {
-	name:'Quay',    /* property: Name Value: string */
+	name:'Quay',    /* Key: Name Value: string */
 	rooms: 40,                        //Value: number
 	booked: 25,                       //Value: number
 	gym: true,                        //Value: Boolean
@@ -24,3 +24,9 @@ FUNCTIONS BECOME KNOWN AS METHODS
 - Methods represent tasks that are associated with the object. For example, you can check 
 how many rooms are available by subtracting the number of booked rooms from the total number
 of rooms. 
+
+KEY
+- Like variables and named functions, properties and methods have a name and value. In an
+object, that name is called a key.
+- An object cannot have two keys with the same name. This is because keys are used to 
+access their corresponding value.
