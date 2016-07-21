64fb23ee9d74e5adc4a461f7e30edef228eac1dd
craig91 <craigdejean1@gmail.com>
2016-07-21 00:32:09 -0400

    methods

---
+++ methods.js
@@ -0,0 +1,19 @@
+var mack = [];
+   mack.push("mack");
+   mack.push("the", "knife");
+     console.log(mack);
+
+console.log(mack.join(" "));
+
+
+console.log(mack.pop());
+
+console.log(mack)
+
+
+
+
+// The push method can be used to add values to the end of an array. 
+// The pop method does exactly the opposite: it removes the value at the end of the array and returns it.
+// An array of strings can be flattened to a single string with the join method. The argument given to join
+// determines the text that is glued between the array's elements.
