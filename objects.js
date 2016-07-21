ef60926b1be4cb8c17f1e835644b5cf12925bf6a
craig91 <craigdejean1@gmail.com>
2016-07-21 01:18:24 -0400

    objects

---
+++ objects.js
@@ -0,0 +1,16 @@
+var day1 = {
+  squirrel: false,
+  events: ["work", "touched tree", "pizza", "running",
+           "television"]
+};
+
+console.log(day1.squirrel);
+console.log(day1.wolf)
+
+day1.wolf = false;
+console.log(day1.wolf);
+
+
+// Inside the curly braces, we can give a list of properties separated by commas. 
+// Each property is written as a name, followed by a colon, followed by an 
+// expression that provides a value for the porperty. 
