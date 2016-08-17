866672801b9dae60005cce39ed1c35c64349cd1a
craig91 <craigdejean1@gmail.com>
2016-08-16 23:14:58 -0400

    Initial Array Methods commit

---
+++ Array_Methods/toString.js
@@ -0,0 +1,16 @@
+// tostring(): returns a string representing the specified array and its elements.
+
+
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"]
+for(var i = 0; i < fruits.length; i++){
+	console.log(fruits[i])
+	fruits.tostring();
+}
+
+
+
+// In the code above, the for loop will iterate though the array fruits
+// and for each time it loops, the values inside the array will be logged 
+// and converted to a string using the toString() method.
