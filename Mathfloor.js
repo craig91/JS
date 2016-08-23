dd9ac9d5c3ae87eb565fee5ee53fc0b9cb4ebac9
craig91 <craigdejean1@gmail.com>
2016-08-23 11:35:26 -0400

    Math Floor Tutorial

---
+++ Mathfloor.js
@@ -0,0 +1,28 @@
+// Math.floor() method
+
+Math.floor(1.6);
+// Run in a console
+// Round a number down to the nearest integer
+
+//The floor method rounds a number downwards to the nearest integer, and returns the result
+// If the argument passed is an integer, the value will not be rounded.
+
+
+
+
+var value = Math.floor(10.3)
+document.write("First test value : " + value)
+
+var value = Math.floor(30.9)
+document.write("Second test value : " + value)
+
+var value = Math.floor(-2.9)
+document.write("Third test value : " + value)
+
+var value = Math.floor(-2.2)
+document.write("Fourth test value : " + value)
+
+
+//returns the largest integer less than or equal to a number.
+
+
