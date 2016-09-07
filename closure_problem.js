0ab3de370b05193f1a93fef082a9da34cfa2fb99
craig91 <craigdejean1@gmail.com>
2016-09-07 11:06:10 -0400

    Difficult problem

---
+++ closure_problem.js
@@ -0,0 +1,19 @@
+var hidden = mystery(3);
+var jumble = mystery3(hidden);
+var result = jumble(2);
+
+function mystery ( input ){
+  var secret = 4;
+  input += 2;
+  function mystery2 ( multiplier ) {
+    multiplier *= input;
+    return secret * multiplier;
+  }
+  return mystery2;
+}
+function mystery3 (param){
+  function mystery4 (bonus){
+    return param(6) + bonus;
+  }
+  return mystery4;
+}
