2e41463956316377a98be4ebd967d522f6cede38
craig91 <craigdejean1@gmail.com>
2016-12-24 14:53:40 -0500

    instructions for creating objects

---
+++ Objects/obj_create.js
@@ -0,0 +1,26 @@
+// Creating Objects
+
+// There are 2 ways to create Objects
+// You can have one empty one and populate it with the object() function or you can create an object literal.
+
+var books = {}
+
+ // or 
+
+var books = {
+    title: '1984',
+    year: 1949,
+    Author: 'George Orwell'
+}
+////////////////////////////////////////////////////////
+
+// This is how you populate the empty object. Do this in a repl to see the effects
+
+var books = {}
+
+books.title = "1984"
+books.year = 1949
+books.Author = 'George Orwell'
+
+console.log(books)
+
