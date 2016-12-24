ca01f906c5ec0580df3c8f7121db8e2ac20bd261
craig91 <craigdejean1@gmail.com>
2016-12-23 20:47:02 -0500

    added basic obj scaffold

---
+++ Objects/Obj_scaffold.js
@@ -0,0 +1,18 @@
+var empty = {};  /// <--- This is an empty object
+
+
+var axis = { // <---- This is a populated object 
+    x : 'o',
+    y : 'o'
+}
+
+var naruto = {  // <---- Object with a person's information
+    age: 21,
+    hometown: "konohagakure",
+    favJutsu: "Kagebunshin no Jutsu"
+}
+
+
+
+// These are object literals. An object literal is an object where the property/name value pairs are separated by commas and enclosed in {}.
+// It is also an expression that creates and initializes a new and distinct object each time it is evaluated.
