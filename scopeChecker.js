b435ae1ea4dca4e4f2947b1401a54ee85c111881
craig91 <craigdejean1@gmail.com>
2017-01-17 18:07:29 -0500

    added nested scope

--- scopeChecker.js
+++ scopeChecker.js
@@ -16,3 +16,15 @@ function checkScope() {
 checkScope()
 document.write(scope)
 document.write(myScope)
+
+
+var scope = "global scope"
+function checkScope () {
+    var scope = "local scope";
+    function nested() {
+        var scope = "nested scope";
+        document.write(scope);
+    }
+    nested();
+}
+checkScope();
