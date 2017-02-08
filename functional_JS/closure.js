31b7933f04c4c3b58da069753bd965be5dd9fe8d
craig91 <craigdejean1@gmail.com>
2017-02-07 20:10:05 -0500

    added closure side effect

--- functional_JS/closure.js
+++ functional_JS/closure.js
@@ -13,3 +13,20 @@ function showName (firstName, lastName) {
 }
 
 showName("Craig", "Dejean")
+
+
+
+function celebrityName(firstName) {
+  var nameIntro = "This celebrity is ";
+   // lastName() has access to celebrityName's variable & params
+  function lastName(theLastName) {
+    return nameIntro + firstName + " " theLastName;
+  }
+  return lastName;
+}
+
+var mjName = celebrityName("Michael");
+
+mjName("Jackson")
+
+// This one was a little tricky but I got it, it is also one of the most important things to understand about closures. The inner function ( lasName() ) still has access to the outer function's variables even after the outer function has returned. So var mjName is kind of hard coding the first name and then the 2nd mj name function call will be the last name, because of the inner function.
